eadb5487c495b4b86e1838164ab4a072b94b26d7
RawenLb <99543648+RawenLb@users.noreply.github.com>
2025-02-19 11:17:49 +0100

    .

---
+++ ProjetMicroService/src/main/java/com/example/projetmicroservice/ProjetMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMicroServiceApplication.class, args);
+	}
+
+}
---
+++ ProjetMicroService/src/test/java/com/example/projetmicroservice/ProjetMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
