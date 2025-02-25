7fd416f4f72b4e66bb4385a7e27969bffef7ff08
RawenLb <99543648+RawenLb@users.noreply.github.com>
2025-02-25 12:32:42 +0100

    PFEmicroServices

---
+++ AccompagnementPFEMicroService/src/test/java/com/example/accompagnementpfemicroservice/AccompagnementPfeMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accompagnementpfemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccompagnementPfeMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
