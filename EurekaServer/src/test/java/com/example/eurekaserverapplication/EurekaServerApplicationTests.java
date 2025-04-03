ac1e8c779f850e62680e594c97619201e6dd53b9
RawenLb <99543648+RawenLb@users.noreply.github.com>
2025-04-03 23:33:31 +0100

    correction code apiGateway (application.yml) et liaison avec microservices

--- EurekaServer/src/test/java/com/example/eurekaserverapplication/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/example/eurekaserverapplication/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package tn.esprit.gateway;
+package com.example.eurekaserverapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class EurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
--- Gateway/src/test/java/com/example/apigatewayservice/ApiGatewayServiceApplicationTests.java
+++ Gateway/src/test/java/com/example/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package tn.esprit.eurekaserver;
+package com.example.apigatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ApiGatewayServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
