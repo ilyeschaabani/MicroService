551a14bd2e428939842836384057ea13f267f31f
RimaDhrai <159543634+Rimadh@users.noreply.github.com>
2025-04-15 06:02:13 +0200

    frontend
    frontend intégrée

---
+++ Frontend/frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AddQuestionComponent } from './Quiz/add-question/add-question.component';
+
+export const routes: Routes = [
+
+
+
+    {  path: 'add-question',  component: AddQuestionComponent }, 
+];
